7122d465565a3e46b0e75c183f40d924271f59d0
Iryna Shumovska <shumovska_ia@groupbwt.com>
2023-04-10 23:46:51 +0300

    add header with navigation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
 <template>
-  <div>
-    <RouterView />
+  <div class="homeContainer">
+    <div className="header">
+      <Navigation />
+      <RouterView />
+    </div>
   </div>
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
+import Navigation from "./components/Navigation.vue"
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+  body {
+  margin: 0;
+  }
+
+  .homeContainer {
+    background-color: #d3d3d3;
+    min-height: 100vh;
+    width: 100%;
+    height: auto;
+  }
+  .header {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,50 @@
+    <template>
+        <div class="headerContainer">
+            <RouterLink :to="{ name: 'home' }" class="logo">
+                <div class="headerLeft">
+                    <img src="../assets/panda.png" class="logoImg"/>
+                    <NavLink to="/" class="logoText">Weather</NavLink>
+                </div>
+            </RouterLink>
+        </div>
+    </template>
+  
+    <script setup>
+    import { RouterLink } from "vue-router";
+    </script>
+  
+    <style scoped>
+        .headerContainer {
+        height: 80px;
+        width: 100%;
+        margin-bottom: 10px;
+        background-color: #b5b5b5;
+        display: flex;
+        align-items: center;
+        position: sticky;
+        top: 0;
+        z-index: 999;
+        }
+        .headerLeft {
+        flex: 2;
+        }
+        .logo {
+        cursor: pointer;
+        text-decoration-line: none;
+        padding: 10px;
+        }
+        .logoText {
+        font-size: 20px;
+        margin-left: 20px;
+        margin-right: 20px;
+        font-weight: bold;
+        color: rgb(0, 0, 0);
+        }
+        .logoImg{
+        height: 50px;
+        align-items: center;
+        display: flex;        
+        margin-left: 30px;
+        margin-right: 30px;
+        }
+    </style>
